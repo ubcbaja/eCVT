799cce910de08bcc9a478082c0c480fe11429c84
Raymond <yuraymond96@gmail.com>
2019-11-04 16:37:01 -0800

    stuff

---
+++ CFP/VL6180X.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  4 16:23:27 2019
+
+Resource link: https://learn.adafruit.com/adafruit-vl6180x-time-of-flight-micro-lidar-distance-sensor-breakout/overview
+
+@author: Safet
+"""
+
+import board
+import busio
+import adafruit_vl6180x
+
+# Stuff
--- Misc/threadingTimerTest.py
+++ Misc/threadingTimerTest.py
@@ -3,8 +3,19 @@
 Created on Mon Nov  4 15:59:56 2019
 
 @author: Safet
+
+Purpose: Create timer and threads to determine resopnse for CFP
+
+Links: 
+    https://www.youtube.com/watch?v=2ZwuKeL0aHs 
+    https://realpython.com/intro-to-python-threading/
+
 """
 
 import logging
 import threading
 import time
+
+def sleeper (n, name):
+    print ('Helloo this is %s\n', name)
+    time.sleep(n)
