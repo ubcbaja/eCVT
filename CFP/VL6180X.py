ca314aa7d0401920d0b83fa27b0a82dabfb83132
Raymond <yuraymond96@gmail.com>
2019-11-04 16:44:24 -0800

    Added resource comments

--- CFP/VL6180X.py
+++ CFP/VL6180X.py
@@ -2,7 +2,9 @@
 """
 Created on Mon Nov  4 16:23:27 2019
 
-Resource link: https://learn.adafruit.com/adafruit-vl6180x-time-of-flight-micro-lidar-distance-sensor-breakout/overview
+Resource links:
+    https://learn.adafruit.com/adafruit-vl6180x-time-of-flight-micro-lidar-distance-sensor-breakout/overview
+    https://circuitpython.readthedocs.io/en/4.x/shared-bindings/busio/__init__.html
 
 @author: Safet
 """
