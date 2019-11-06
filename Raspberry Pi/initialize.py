a2a557c43b8a9d8d4d2d5e20b4f9ac2b84dc2857
Raymond <yuraymond96@gmail.com>
2019-11-06 14:14:53 -0800

    Initial commit

---
+++ Raspberry Pi/initialize.py
@@ -0,0 +1,27 @@
+# import pandas as pd
+
+import board
+import digitalio
+import busio
+import adafruit_vl6180x
+
+print("Verifying that CircuitPython modules with VL6180X installed properly...")
+
+# Create digital input
+pin = digitalio.DigitalInOut(board.D4)
+print("Digital IO ok!")
+
+# Create I2C device
+i2c = busio.I2C(board.SCL, board.SDA)
+print("I2C ok!")
+
+# Create SPI device
+spi = busio.SPI(board.SCLK, board.MOSI, board.MISO)
+print("SPI ok!")
+
+# Create VL6180X sensor
+try:
+    sensor = adafruit_vl6180x.VL6180X(i2c) # link to I2C
+    print("VL6180X ok!")
+except ValueError:
+    print("No I2C device connected!")
