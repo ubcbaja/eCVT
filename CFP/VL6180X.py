778fc699318babf94ee717b3403cdb7da74986a9
Raymond <yuraymond96@gmail.com>
2019-11-12 14:25:23 -0800

    Added live range printout in addition to plot

--- CFP/VL6180X.py
+++ CFP/VL6180X.py
@@ -52,7 +52,7 @@ try:
     while True:
         def animate(i, ys):
             range = sensor.range
-            
+            print("Range: {0}mm".format(range), end = '\r')
             ys.append(range) # add to plot
             ys = ys[-xLen:] # limit y data
             
