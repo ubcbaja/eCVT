1fe5f455596c1b09f35d7dabca5bc7b5af7b4f0d
Raymond <yuraymond96@gmail.com>
2019-11-12 13:22:13 -0800

    Added animations to VL6180X

--- CFP/VL6180X.py
+++ CFP/VL6180X.py
@@ -5,7 +5,7 @@ Created on Mon Nov  4 16:23:27 2019
 Resource links:
     https://learn.adafruit.com/adafruit-vl6180x-time-of-flight-micro-lidar-distance-sensor-breakout/overview
     https://circuitpython.readthedocs.io/en/4.x/shared-bindings/busio/__init__.html
-
+    https://learn.sparkfun.com/tutorials/graph-sensor-data-with-python-and-matplotlib/speeding-up-the-plot-animation
 @author: Raymond
 """
 
@@ -13,10 +13,13 @@ import board
 import digitalio
 import busio
 import adafruit_vl6180x
+import numpy as np
 import time
 import os
 import sys
 import datetime
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 # Configure I2C port
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -27,13 +30,40 @@ try:
 except ValueError:
     print("Sensor not detected! Is an I2C device connected?")
 
+# Set up variables for plotting
+DAQRate = 10
+xLen = 101 # 101 datapoints per plot
+yRange = [0, 100] # Y range from 0 to 100mm
+
+# Set up plot graph
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+xs = list(np.linspace(0, DAQRate, xLen)) # x data (time)
+ys = [0] * xLen # y data (distance)
+
+line, = ax.plot(xs, ys) # unpack tuple to take first element only
+
+def animate(i, ys):
+    range = sensor.range
+    
+    ys.append(range) # add to plot
+    ys = ys[-xLen:] # limit y data
+    
+    line.set_ydata(ys)
+    
+    return line
+
+animashun = animation.FuncAnimation(fig, animate, fargs = (ys,), interval = 50, blit = True)
+plt.show()
+
+"""
 try:
     while True:
         range = sensor.range
         lums = sensor.read_lux(adafruit_vl6180x.ALS_GAIN_1)
         print("Range: {0}mm\t Light: {0}lux".format(range, lums), end = "\r")
-        time.sleep(0.05)
+        
+        time.sleep(1/DAQRate) # 10 Hz DAQ rate
 except KeyboardInterrupt:
     print("\nKeyboard interrupt detected, stopping readings...")
-
-print("This is the line after")
+"""
---
+++ Misc/pythonTest.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 13:05:06 2019
+
+@author: Safet
+"""
+
+import numpy as np
+
+x = list(np.linspace(0, 10, 1001))
+
+#print(x)
+
+xs = list(range(0, 200))
+
+print(xs)
