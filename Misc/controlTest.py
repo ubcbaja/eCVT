bfd9b572877e72a66b75ab7f6759eeac86ce80ce
Raymond <yuraymond96@gmail.com>
2019-12-20 11:40:45 -0800

    Create controlTest.py

---
+++ Misc/controlTest.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 20 11:34:02 2019
+
+@author: Safet
+
+Test code to control servo based on SEL distance.
+
+Sauce:
+    https://projects.raspberrypi.org/en/projects/grandpa-scarer/4
+
+"""
+
+import RPi.GPIO as GPIO
+from time import sleep
