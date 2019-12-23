4f38ab609bb3e6846233f5b466e19f545f793670
Raymond <yuraymond96@gmail.com>
2019-12-23 00:47:58 -0800

    Organized error check to separate header

--- Misc/controlTestATMega/src/main.cpp
+++ Misc/controlTestATMega/src/main.cpp
@@ -5,8 +5,9 @@
 */
 
 #include <Arduino.h>
-#include <Adafruit_VL6180X.h>
-#include <Wire.h>
+// #include <Adafruit_VL6180X.h>
+// #include <Wire.h>
+#include "resources.h" // includes Adafruit and Wire libraries
 
 Adafruit_VL6180X vl = Adafruit_VL6180X();
 
@@ -22,7 +23,7 @@ void setup() {
 }
 
 void loop() {
-  float lux = vl.readLux(VL6180X_ALS_GAIN_5);
+  float lux = vl.readLux(VL6180X_ALS_GAIN_20);
   
   uint8_t range = vl.readRange();
   uint8_t status = vl.readRangeStatus();
@@ -32,35 +33,6 @@ void loop() {
     Serial.print("\tLux:\t"); Serial.println(lux);
   }
 
-  // // Some error occurred, print it out!
-  
-  if  ((status >= VL6180X_ERROR_SYSERR_1) && (status <= VL6180X_ERROR_SYSERR_5)) {
-    Serial.println("System error");
-  }
-  else if (status == VL6180X_ERROR_ECEFAIL) {
-    Serial.println("ECE failure");
-  }
-  else if (status == VL6180X_ERROR_NOCONVERGE) {
-    Serial.println("No convergence");
-  }
-  else if (status == VL6180X_ERROR_RANGEIGNORE) {
-    Serial.println("Ignoring range");
-  }
-  else if (status == VL6180X_ERROR_SNR) {
-    Serial.println("Signal/Noise error");
-  }
-  else if (status == VL6180X_ERROR_RAWUFLOW) {
-    Serial.println("Raw reading underflow");
-  }
-  else if (status == VL6180X_ERROR_RAWOFLOW) {
-    Serial.println("Raw reading overflow");
-  }
-  else if (status == VL6180X_ERROR_RANGEUFLOW) {
-    Serial.println("Range reading underflow");
-  }
-  else if (status == VL6180X_ERROR_RANGEOFLOW) {
-    Serial.println("Range reading overflow");
-  }
-  delay(50);
-
+  // Some error occurred, print it out!
+  CheckErrors(status);
 }
---
+++ Misc/controlTestATMega/src/resources.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stdint.h>
+#include <Adafruit_VL6180X.h>
+#include <Wire.h>
+
+void CheckErrors(uint8_t status) {
+    if  ((status >= VL6180X_ERROR_SYSERR_1) && (status <= VL6180X_ERROR_SYSERR_5)) {
+        Serial.println("System error");
+    }
+    else if (status == VL6180X_ERROR_ECEFAIL) {
+        Serial.println("ECE failure");
+    }
+    else if (status == VL6180X_ERROR_NOCONVERGE) {
+        Serial.println("No convergence");
+    }
+    else if (status == VL6180X_ERROR_RANGEIGNORE) {
+        Serial.println("Ignoring range");
+    }
+    else if (status == VL6180X_ERROR_SNR) {
+        Serial.println("Signal/Noise error");
+    }
+    else if (status == VL6180X_ERROR_RAWUFLOW) {
+        Serial.println("Raw reading underflow");
+    }
+    else if (status == VL6180X_ERROR_RAWOFLOW) {
+        Serial.println("Raw reading overflow");
+    }
+    else if (status == VL6180X_ERROR_RANGEUFLOW) {
+        Serial.println("Range reading underflow");
+    }
+    else if (status == VL6180X_ERROR_RANGEOFLOW) {
+        Serial.println("Range reading overflow");
+    }
+}
