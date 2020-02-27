3253471b7277a5e5af802aa73a514c02b21acb47
Raymond <yuraymond96@gmail.com>
2020-02-26 23:34:11 -0800

    Auto tune found PID values

--- Controls/ModelParams.m
+++ Controls/ModelParams.m
@@ -1,16 +1,16 @@
 % Plant parameters
-Je = 10;
-Be = 5;
-La = 1;
+Je = 10e-4;
+Be = 0.01;
+La = 0.001;
 Kt = 1;
-Ra = 1;
+Ra = 5;
 Kw = 1;
 d = 1;
 theta = 1;
 m = 1;
-K = 1;
+K = 100;
 
 % PID gains
-Ki = 1;
-Kp = 1;
-Kd = 0;
+Ki = 1.469e4;
+Kp = 4362;
+Kd = 235.7;
