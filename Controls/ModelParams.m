bd24511b9db1900e7e20790af74fa24b56e08d91
Raymond <yuraymond96@gmail.com>
2020-03-15 16:22:34 -0700

    Remodelled system using Laplace functions

--- Controls/ModelParams.m
+++ Controls/ModelParams.m
@@ -1,14 +1,14 @@
 % Plant parameters
-Je = 10e-4;
-Be = 0.01;
-La = 0.001;
-Kt = 1;
-Ra = 5;
-Kw = 1;
-d = 0.05;
-theta = 1;
-m = 1;
-K = 100;
+Jr = 0.0001;    % estimate                  [kgm^2]
+Je = 0.01;      % estimate                  [kgm^2]
+Be = 0.001;     % estimate                  [Nm/(rad/s)]
+La = 0.02;      % estimate                  [H]
+Ra = 2;         % estimate                  [ohm]
+Kt = 0.283;     % torque constant           [Nm/A]
+Kv = 0.105;     % voltage constant          [Vs/rad]
+r = 0.005;      % moment arm                [m]
+Kspring = 250;  % estimate, spring constant [N/m]
+Kpot = 0.2653;  % estimate, pot constant    [V/rad]
 
 % PID gains
 Kp = 111.1;
