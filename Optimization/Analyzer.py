353509e98eaf17de4535bcfd249c7b6de90fc03d
Raymond <yuraymond96@gmail.com>
2020-03-14 20:20:41 -0700

    Update Analyzer.py

--- Optimization/Analyzer.py
+++ Optimization/Analyzer.py
@@ -52,21 +52,21 @@ xOpt, fOpt = pso(rastrigin, lowBound, upBound, swarmsize=10, maxiter=100)
 print("Optimal vals found at: " + str(xOpt) + ", " + str(fOpt))
 
 #%% Graph Rastrigin
-X = np.linspace(-5.12, 5.12, 100)     
-Y = np.linspace(-5.12, 5.12, 100)     
-X, Y = np.meshgrid(X, Y) 
+# X = np.linspace(-5.12, 5.12, 100)     
+# Y = np.linspace(-5.12, 5.12, 100)     
+# X, Y = np.meshgrid(X, Y) 
 
-Z = (X**2 - 10 * np.cos(2 * np.pi * X)) + \
-  (Y**2 - 10 * np.cos(2 * np.pi * Y)) + 20
+# Z = (X**2 - 10 * np.cos(2 * np.pi * X)) + \
+#   (Y**2 - 10 * np.cos(2 * np.pi * Y)) + 20
  
-fig = plt.figure() 
-ax = fig.gca(projection='3d') 
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
-  cmap=cm.nipy_spectral, linewidth=0.08,
-  antialiased=True)    
-ax.scatter(xOpt[0], xOpt[1], 0, s=14, c="red")
-# plt.savefig('rastrigin_graph.png')
-plt.show()
+# fig = plt.figure() 
+# ax = fig.gca(projection='3d') 
+# ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+#   cmap=cm.nipy_spectral, linewidth=0.08,
+#   antialiased=True)    
+# ax.scatter(xOpt[0], xOpt[1], 0, s=14, c="red")
+# # plt.savefig('rastrigin_graph.png')
+# plt.show()
 
 #%% GA Test
 # """
